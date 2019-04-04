3f1c052d3b331d8b4e0f78a904816ce7ed35ae7a
Rebecca <ph.rdang@gmail.com>
2019-04-03 17:26:55 -0700

    Initial commit matrix.py

---
+++ matrix.py
@@ -0,0 +1,23 @@
+'''
+Author: Rebecca Dang
+Date Started: April 2, 2019
+Date Completed (Ver 1): [in progress]
+
+Program Objectives:
+Use python classes to:
+(be able to take in user input for these)
+1. Create and store matrices
+    -subclasses: rows, cols
+
+    -init method
+    -str method
+
+    -get rows
+    -get cols
+
+    -add/subtract matrices
+    -multiply matrices
+
+    -find cofactors, minors, determinants
+2. Be able to encode and decode messages using matrices
+'''
